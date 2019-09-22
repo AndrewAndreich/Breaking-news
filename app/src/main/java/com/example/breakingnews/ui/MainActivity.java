8c061f1ba8dd5af89a7f54376bdfa48b75ef703c
AndrewAndreich <andrei.gorkin@yandex.ru>
2019-09-22 15:36:28 +0300

    Added recyclerview click listener

--- app/src/main/java/com/example/breakingnews/ui/MainActivity.java
+++ app/src/main/java/com/example/breakingnews/ui/MainActivity.java
@@ -1,6 +1,8 @@
-package com.example.breakingnews;
+package com.example.breakingnews.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,9 +10,15 @@ import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.breakingnews.BuildConfig;
+import com.example.breakingnews.R;
 import com.example.breakingnews.adapters.CategoriesAdapter;
 import com.example.breakingnews.api.RetrofitClientInstance;
+import com.example.breakingnews.api.response.Article;
 import com.example.breakingnews.api.response.BaseResponse;
+import com.example.breakingnews.util.RecyclerItemClickListener;
+
+import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -20,6 +28,10 @@ public class MainActivity extends AppCompatActivity {
 
     private RecyclerView categoriesList;
     private CategoriesAdapter categoriesAdapter;
+
+    static final String IMAGE_URL_KEY = "image_url";
+    static final String TITLE_KEY = "title";
+    static final String CONTENT_KEY = "content";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,5 +59,21 @@ public class MainActivity extends AppCompatActivity {
                         Toast.makeText(MainActivity.this, "Error: " + t.getMessage(), Toast.LENGTH_LONG).show();
                     }
                 });
+
+        setClickListener();
+    }
+
+    private void setClickListener() {
+        categoriesList.addOnItemTouchListener(new RecyclerItemClickListener(this, categoriesList, new RecyclerItemClickListener.OnItemClickListener() {
+            @Override
+            public void onItemClick(View view, int position) {
+                Intent intent = new Intent(MainActivity.this, NewsDetailsActivity.class);
+                List<Article> articles = categoriesAdapter.getDataList();
+                intent.putExtra(IMAGE_URL_KEY, articles.get(position).getUrlToImage());
+                intent.putExtra(TITLE_KEY, articles.get(position).getTitle());
+                intent.putExtra(CONTENT_KEY, articles.get(position).getDescription());
+                startActivity(intent);
+            }
+        }));
     }
 }
